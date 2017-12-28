fc31437eb3b4f268a6f8df1186eed2c4c27673fb
Ted Diepenbrock <unoirok08@gmail.com>
2017-12-28 01:03:46 -0600

    Add font-awesome library and about me button

--- js/hlo.js
+++ js/hlo.js
@@ -51,56 +51,61 @@ $(function() {
 });
 
 
+// animated popups
+
+
+
+
 
 // Spotlight effect
-$('.searchlight')
-    .on('mousemove', function(event) {
-        $(this).addClass('on').css({'margin-left': event.pageX - 150, 'margin-top': event.pageY - 150});
-    })
-    .on('mouseout', function(event) {
-        $(this).removeClass('on');
-    })
-    .on('click', function() {
-        $(this).fadeOut(function() {
-            $(this).remove();
-        });
-    });
-
-
-
-
-
-// fancyBox v3.2
-$('#test').on('click',function() {
-    
-    $.fancybox.open([
-        {
-            src: '../../healthcarelib/images/a1.jpg',
-            opts: {
-                caption: 'First caption',
-                thumb: '../../healthcarelib/images/a1.jpg'
-            }
-        },
-        {
-            src: './images/a2.jpg',
-            opts: {
-                caption: 'Second caption',
-                thumb: './images/a2.jpg'
-            }
-        },
-        {
-            src: './images/a3.jpg',
-            opts: {
-                caption: 'Third caption',
-                thumb: './images/a3.jpg'
-            }
-        }
-    ], {
-        loop: true,
-        hash: 'test'
-    });
-
-});
+//$('.searchlight')
+//    .on('mousemove', function(event) {
+//        $(this).addClass('on').css({'margin-left': event.pageX - 150, 'margin-top': event.pageY - 150});
+//    })
+//    .on('mouseout', function(event) {
+//        $(this).removeClass('on');
+//    })
+//    .on('click', function() {
+//        $(this).fadeOut(function() {
+//            $(this).remove();
+//        });
+//    });
+//
+//
+//
+//
+//
+//// fancyBox v3.2
+//$('#test').on('click',function() {
+//    
+//    $.fancybox.open([
+//        {
+//            src: '../../healthcarelib/images/a1.jpg',
+//            opts: {
+//                caption: 'First caption',
+//                thumb: '../../healthcarelib/images/a1.jpg'
+//            }
+//        },
+//        {
+//            src: './images/a2.jpg',
+//            opts: {
+//                caption: 'Second caption',
+//                thumb: './images/a2.jpg'
+//            }
+//        },
+//        {
+//            src: './images/a3.jpg',
+//            opts: {
+//                caption: 'Third caption',
+//                thumb: './images/a3.jpg'
+//            }
+//        }
+//    ], {
+//        loop: true,
+//        hash: 'test'
+//    });
+//
+//});
 
 
 
